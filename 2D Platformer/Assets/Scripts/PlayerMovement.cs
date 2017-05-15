fd6840e37bb2d25a712b88be1baa61fee352a663
Joppe van Basten <21983@ma-web.nl>
2017-05-15 13:55:18 +0200

    Bewegen en (springen)
    bewegen en springen een beetje

---
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+    public float maxSpeed = 5f;
+    public float jumpHeight = 20f;
+    private bool Jump = false; //my nama jeff
+
+    private Rigidbody2D rb2d;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+        }
+
+        if (Input.GetKeyDown("space") && !Jump)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(rb2d.velocity.x, jumpHeight);
+            Jump = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Platform")
+            Jump = false;
+
+    }
+
+    /*
+        bool canJump()
+        {
+            Ray raycast = new Ray(transform.position, -Vector3.up);
+            return Physics.Raycast(raycast, 0.5f);
+        }
+        */
+}
